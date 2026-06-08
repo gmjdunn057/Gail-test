46fabe2823521919f328e6fdd739cd06d696a64a
gmjdunn057 <gmjdunn@gmail.com>
2026-06-08 15:02:56 -0500

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,9 @@ if 'secret' not in st.session_state:
 guess = st.number_input('Input a number between 1 and 100',min_value=1, max_value=100)
 if st.button('Submit Guess'):
     st.write('You guessed '(str(guess)))
-if guess< st.session_state.secret:
+if guess<st.session_state.secret:
     st.warning('Sorry, too low')
-    elif guess> st.session_state.secret:
+    elif guess>st.session_state.secret:
         st.warning('Sorry, too high')
     else:
         st.success('Yay! You guessed it!')
