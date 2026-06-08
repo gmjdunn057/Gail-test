d2a91eafda5bfd1eb47f15ad1e5be42219d7d113
gmjdunn057 <gmjdunn@gmail.com>
2026-06-08 14:53:09 -0500

    added code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
 import streamlit as st
+import random
 
-st.title("🎈 My new app")
+st.title("🎈 My test app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Guess a number between 1-100"
 )
+if 'secret' not in st.session_state:
+    st.session_state.secret = random.randint(1,100)
+    st.session_state.guess = False
+
+guess = st.number_input('Input a number between 1 and 100',min_value=1, max_value=100)
+if st.button('Submit Guess'):
+(        st.write('You guessed '(str(guess))
+if guess , st,sessuib_state.secret:
+    st.warning('Sorry, too low')
+    elif guess. st,sessuib_state.secret:
+        st.warning('Sorry, too high')
+    else:
+        st.success(Yay! You guessed it!')
+if guess==st.session_state.secret:
+    if st.button('Play again'):
+        st.session_state.secret = random.ranint(1,100)
+        st.rerun()    
